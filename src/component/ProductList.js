cf2d09679547421560d0e9d9c575679fbdfcdbcc
Ashish M J <ashm.jagadeesh@gmail.com>
2021-03-11 13:25:00 +0530

    read json data

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,12 +1,21 @@
 import React, { Component,useEffect } from 'react'
 import ProductRow from './ProductRow';
 import products from './data'
+import axios from 'axios';
 export default class ProductList extends Component {
 ;
     state={
           products:products
     }
-    
+    componentDidMount(){
+        console.log("component did mount");
+        axios.get("http://localhost:8081/products/").
+        then(response=>{
+            console.log("got response");
+            const products=response.data;
+            this.setState({products});
+        }).catch(err=>console.log(err))
+    }
     render() {
         return (
             <div className="productList">
